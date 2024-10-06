996859ecc8c1184e9472d4fe9755e9b6aff0923d
Mikko Partio <mikko.partio@fmi.fi>
2024-10-06 19:52:41 +0300

    Support reading hourly data ie target is initialization+60min

--- util.py
+++ util.py
@@ -3,21 +3,41 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 
 
-def read_data(n_hist=1, n_futu=1, dataset_size="10k", batch_size=8):
-    train_loader, val_loader = None, None
+def read_data(n_hist=1, n_futu=1, dataset_size="10k", batch_size=8, hourly=False):
 
-    n_hist = 1
-    n_futu = 1
+    def hourly_split_1_1(data):
+        # special case when n_hist = 1 and n_futu = 1 and hourly = True
+        T, H, W, C = data.shape
+        N = (T // 4) * 4
+        data = data[:N, ...]
+        data = data.reshape(-1, 4, H, W, C).transpose(1, 0, 2, 3, 4).reshape(-1, H, W, C)
+        x_data = np.expand_dims(data[1::2,], axis=1)
+        y_data = np.expand_dims(data[::2], axis=1)
+        return x_data, y_data
+
+    def instant_split(data, s_len):
+        T, H, W, C = data.shape
+        N = (T // s_len) * s_len
+        data = data[:N, ...]
+        data = data.reshape(N // s_len, s_len, H, W, C)
+        x_data = data[:, :n_hist, ...]
+        y_data = data[:, n_hist : n_hist + n_futu, ...]
+        return x_data, y_data
+
+    train_loader, val_loader = None, None
 
     for ds in ("train", "val"):
         s_len = n_hist + n_futu
 
         data = np.load(f"{ds}-{dataset_size}.npz")["arr_0"]
-        d_len = (data.shape[0] // s_len) * s_len
-        data = data[:d_len, ...]
-        data = data.reshape(d_len // s_len, s_len, data.shape[1], data.shape[2], 1)
-        x_data = data[:, :n_hist, ...]
-        y_data = data[:, n_hist : n_hist + n_futu, ...]
+
+        if not hourly:
+            x_data, y_data = instant_split(data, s_len)
+        else:
+            if n_hist == 1 and n_futu == 1:
+                x_data, y_data = hourly_split_1_1(data)
+            else:
+                raise ValueError("hourly split only works for n_hist = 1 and n_fut = 1")
 
         x_data = torch.tensor(x_data, dtype=torch.float32)
         y_data = torch.tensor(y_data, dtype=torch.float32)
