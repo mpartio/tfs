07871c3f2389c5eee5579442c9d74671aa4f54b1
Mikko Partio <mikko.partio@fmi.fi>
2024-10-06 19:54:27 +0300

    Scale losses outside main loss function

--- cc2bmm-train.py
+++ cc2bmm-train.py
@@ -8,15 +8,14 @@ import json
 from datetime import datetime
 from torch.utils.data import DataLoader, TensorDataset
 from mbetanll import MixtureBetaNLLLoss
+from ssim import SSIMLoss
 from cc2bmm import CloudCastV2
 from tqdm import tqdm
 from config import get_args
 from util import *
+from fft_hfc import fft_hfc
 
 args = get_args()
-
-# args.load_model_from = "models/cc2bmm-model.pth"
-# args.save_model_to = "models/cc2bmm-model.pth"
 
 if args.run_name is None:
     args.run_name = randomname.get_name()
@@ -29,6 +28,31 @@ with open(f"{run_dir}/{datetime.now().strftime('%Y%m%d%H%M%S')}-config.json", "w
     json.dump(vars(args), f)
 
 
+def mean_and_var(alpha, beta, weights):
+    mean = alpha / (alpha + beta)
+    var = (alpha * beta) / ((alpha + beta) ** 2 * (alpha + beta + 1))
+
+    weighted_mean = torch.sum(weights * mean, dim=-1, keepdim=True)
+    weighted_var = torch.sum(weights * var, dim=-1, keepdim=True)
+
+    # Combine both the internal variance of each Beta distribution
+    # (weighted by its corresponding weight) and the variance between
+    # the means of the distributions (also weighted by the weights).
+
+    mixture_var = weighted_var + torch.sum(
+        weights * (mean - weighted_mean) ** 2, dim=-1, keepdim=True
+    )
+
+    return weighted_mean, mixture_var
+
+
+def combine_losses(beta, ssim, l1):
+    bw = 0.05
+    sw = 0.5
+    lw = 10
+    return beta * bw + ssim * sw + l1 * lw
+
+
 def roll_forecast(model, x, y, steps):
     assert y.shape[1] == steps
 
@@ -36,26 +60,44 @@ def roll_forecast(model, x, y, steps):
     cumulative_beta_loss = 0.0
     cumulative_var_reg = 0.0
     cumulative_recon_loss = 0.0
-    cumulative_beta_l2 = 0.0
+    cumulative_l1_loss = 0.0
 
     for i in range(steps):
         y_true = y[:, i, ...].unsqueeze(1)
         assert torch.isnan(x).sum() == 0, "NaN in input x"
-        total_loss, beta_loss, var_reg, recon_loss, beta_l2_loss = criterion(*model(x), y_true)
+
+        alpha, beta, weights = model(x)
+
+        beta_loss = beta_criterion(alpha, beta, weights, y_true)
+        mean, var = mean_and_var(alpha, beta, weights)
+
+        recon_loss = ssim_criterion(mean, y_true)
+        l1_loss = l1_criterion(mean, y_true)
+
+        total_loss = combine_losses(beta_loss, recon_loss, l1_loss)
 
         cumulative_loss += total_loss
         cumulative_beta_loss += beta_loss
-        cumulative_var_reg += var_reg
         cumulative_recon_loss += recon_loss
-        cumulative_beta_l2 += beta_l2_loss
+        cumulative_l1_loss += l1_loss
 
+    assert steps == 1
     cumulative_loss /= steps
     cumulative_beta_loss /= steps
-    cumulative_var_reg /= steps
     cumulative_recon_loss /= steps
-    cumulative_beta_l2 /= steps
+    cumulative_l1_loss /= steps
 
-    return cumulative_loss, cumulative_beta_loss, cumulative_var_reg, cumulative_recon_loss, cumulative_beta_l2
+    return (
+        cumulative_loss,
+        cumulative_beta_loss,
+        cumulative_recon_loss,
+        cumulative_l1_loss,
+        alpha,
+        beta,
+        weights,
+        mean,
+        var,
+    )
 
 
 training_start = datetime.now()
@@ -68,6 +110,9 @@ print("Using device", device)
 model = CloudCastV2(dim=args.dim, patch_size=args.patch_size)
 
 print(model)
+
+print("Run name is ", args.run_name)
+
 num_params = count_trainable_parameters(model)
 print(f"Number of trainable parameters: {num_params:,}")
 
@@ -84,10 +129,12 @@ except FileNotFoundError:
 model = model.to(device)
 
 train_loader, val_loader = read_data(
-    dataset_size=args.dataset_size, batch_size=args.batch_size
+    dataset_size=args.dataset_size, batch_size=args.batch_size, hourly=True
 )
 
-criterion = MixtureBetaNLLLoss()
+beta_criterion = MixtureBetaNLLLoss()
+ssim_criterion = SSIMLoss()
+l1_criterion = nn.L1Loss()
 
 lr = 2e-5 if not found_existing_model else 2e-6
 
@@ -123,7 +170,7 @@ for epoch in range(1, args.epochs + 1):
         optimizer.zero_grad()
 
         with autocast_context():  # torch.autocast(device_type="cuda", dtype=torch.float16):
-            loss, _, _, _, _ = roll_forecast(model, inputs, targets, 1)
+            loss, _, _, _, _, _, _, _, _ = roll_forecast(model, inputs, targets, 1)
             train_loss += loss.item()
 
         if use_amp:
@@ -147,33 +194,56 @@ for epoch in range(1, args.epochs + 1):
     total_beta_loss = 0.0
     total_var_reg = 0.0
     total_recon_loss = 0.0
-    total_beta_l2 = 0.0
+    total_l1_loss = 0.0
+    total_fft_hfc_pred = 0.0
+    total_fft_hfc_true = 0.0
+    total_mean = 0.0
+    total_var = 0.0
 
     with torch.no_grad():
 
         for inputs, targets in val_loader:
             inputs, targets = inputs.to(device), targets.to(device)
 
-            alpha, beta, weights = model(inputs)
-            loss, beta_loss, var_reg, recon_loss, beta_l2_loss = criterion(
-                alpha, beta, weights, targets
-            )
+            (
+                loss,
+                beta_loss,
+                recon_loss,
+                l1_loss,
+                alpha,
+                beta,
+                weights,
+                mean,
+                var,
+            ) = roll_forecast(model, inputs, targets, 1)
+
             alpha_s.append(torch.mean(alpha, dim=0).cpu().numpy())
             beta_s.append(torch.mean(beta, dim=0).cpu().numpy())
             weights_s.append(torch.mean(weights, dim=0).cpu().numpy())
 
             val_loss += loss.item()
             total_beta_loss += beta_loss.item()
-            total_var_reg += var_reg.item()
             total_recon_loss += recon_loss.item()
-            total_beta_l2 += beta_l2_loss.item()
+            total_l1_loss += l1_loss.item()
+
+            total_fft_hfc_pred += fft_hfc(mean.cpu().numpy())
+            total_fft_hfc_true += fft_hfc(targets.cpu().numpy())
+
+            total_mean += torch.mean(mean).item()
+            total_var += torch.mean(var).item()
 
             assert np.isnan(val_loss) == 0, "NaN in validation loss"
+
     val_loss /= len(val_loader)
     total_beta_loss /= len(val_loader)
-    total_var_reg /= len(val_loader)
     total_recon_loss /= len(val_loader)
-    total_beta_l2 /= len(val_loader)
+    total_l1_loss /= len(val_loader)
+    total_mean /= len(val_loader)
+    total_var /= len(val_loader)
+
+    total_fft_hfc_pred = total_fft_hfc_pred.item() / len(val_loader)
+    total_fft_hfc_true = total_fft_hfc_true.item() / len(val_loader)
+    fft_hfc_ratio = total_fft_hfc_true / total_fft_hfc_pred
 
     alpha_s = np.asarray(alpha_s).mean(axis=(0, 1, 2, 3))
     beta_s = np.asarray(beta_s).mean(axis=(0, 1, 2, 3))
@@ -192,13 +262,14 @@ for epoch in range(1, args.epochs + 1):
         lr_scheduler.step(val_loss)  # Reduce LR if validation loss has plateaued
 
     print(
-        "Epoch [{}/{}], current best: {}. Train Loss: {:.6f} Val Loss: {:.6f} {} LRx1M: {:.3f}".format(
+        "Epoch [{}/{}], current best: {}. Train Loss: {:.6f} Val Loss: {:.6f} {} fft: {:.3f} LRx1M: {:.3f}".format(
             epoch,
             args.epochs,
             min_loss_epoch,
             train_loss,
             val_loss,
             format_beta(alpha_s, beta_s, weights_s),
+            fft_hfc_ratio,
             1e6 * optimizer.param_groups[0]["lr"],
         )
     )
@@ -223,9 +294,13 @@ for epoch in range(1, args.epochs + 1):
         "duration": (epoch_end_time - epoch_start_time).total_seconds(),
         "lr": optimizer.param_groups[0]["lr"],
         "beta_loss": total_beta_loss,
-        "var_reg": total_var_reg,
         "recon_loss": total_recon_loss,
-        "beta_l2_loss": total_beta_l2,
+        "l1_loss": total_l1_loss,
+        "fft_hfc_pred": total_fft_hfc_pred,
+        "fft_hfc_true": total_fft_hfc_true,
+        "fft_hfc_ratio": fft_hfc_ratio,
+        "mean": total_mean,
+        "var": total_var,
         "saved": saved,
     }
 
--- mbetanll.py
+++ mbetanll.py
@@ -3,39 +3,10 @@ import torch.nn as nn
 import torch.distributions as dist
 import torch.nn.functional as F
 
-# from betanll import BetaNLLLoss
-
 
 class MixtureBetaNLLLoss(nn.Module):
     def __init__(self):
         super(MixtureBetaNLLLoss, self).__init__()
-
-    def predicted_mean_and_var(self, alpha, beta, weights):
-        mean = alpha / (alpha + beta)
-        var = (alpha * beta) / ((alpha + beta) ** 2 * (alpha + beta + 1))
-
-        weighted_mean = torch.sum(weights * mean, dim=-1, keepdim=True)
-        weighted_var = torch.sum(weights * var, dim=-1, keepdim=True)
-
-        # Combine both the internal variance of each Beta distribution
-        # (weighted by its corresponding weight) and the variance between
-        # the means of the distributions (also weighted by the weights).
-
-        mixture_var = weighted_var + torch.sum(
-            weights * (mean - weighted_mean) ** 2, dim=-1, keepdim=True
-        )
-
-        return weighted_mean, mixture_var
-
-    def l1_loss(self, predicted_mean, y_true):
-
-        assert predicted_mean.shape == y_true.shape, (
-            f"Predicted mean shape: {predicted_mean.shape}, "
-            f"True target shape: {y_true.shape}"
-        )
-        loss = F.l1_loss(predicted_mean, y_true, reduction="mean")
-
-        return loss
 
     def beta_nll_loss(self, alpha, beta, weights, y_true):
         y_true_c = y_true.clamp(1e-6, 1 - 1e-6)
@@ -54,32 +25,8 @@ class MixtureBetaNLLLoss(nn.Module):
 
         return loss
 
-    def forward(
-        self,
-        alpha,
-        beta,
-        weights,
-        y_true,
-        beta_weight=0.2,
-        endpoint_reg_weight=0.1,
-        var_reg_weight=8,
-    ):
+    def forward(self, alpha, beta, weights, y_true):
 
-        beta_loss = self.beta_nll_loss(alpha, beta, weights, y_true)
+        beta_loss = self.beta_nll_loss(alpha, beta, weights, y_true).mean()
 
-        predicted_mean, predicted_var = self.predicted_mean_and_var(
-            alpha, beta, weights
-        )
-
-        #        mae_loss = self.l1_loss(predicted_mean, y_true)
-
-        endpoint_reg = torch.mean(
-            (predicted_mean - 0) ** 2 * (predicted_mean > 0.1).float()
-        ) + torch.mean((predicted_mean - 1) ** 2 * (predicted_mean < 0.9).float())
-        var_reg = torch.mean(predicted_var)
-
-        return (
-            beta_weight * beta_loss.mean()
-            + endpoint_reg_weight * endpoint_reg
-            + var_reg_weight * var_reg
-        )
+        return beta_loss
