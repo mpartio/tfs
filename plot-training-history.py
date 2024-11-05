9c00b6763b279978669bcf62fd2ecaa1e716144e
Mikko Partio <mikko.partio@fmi.fi>
2024-11-05 17:28:15 +0200

    Logging

--- plot-training-history.py
+++ plot-training-history.py
@@ -8,6 +8,7 @@ if args.run_name is None:
     raise ValueError("Please provide a run name")
 
 files = read_training_history(args.run_name, args.latest_only)
+print("Read {} files".format(len(files)))
 files = plot_training_history(files, directory=f"runs/{args.run_name}")
 
 for file in files:
