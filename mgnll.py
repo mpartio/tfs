8b85607d768da95ba30b66c8099208aa4d32a4fa
Mikko Partio <mikko.partio@fmi.fi>
2024-11-09 18:38:07 +0000

    Remove double-softplus

--- mgnll.py
+++ mgnll.py
@@ -6,10 +6,7 @@ class MixtureGaussianNLLLoss(nn.Module):
     def __init__(self):
         super(MixtureGaussianNLLLoss, self).__init__()
 
-    def gaussian_nll_loss(self, means, std_devs, weights, y_true):
-        # Clamping std_devs to avoid log(0) and division by zero
-        #std_devs = std_devs.clamp(min=1e-6)
-
+    def forward(self, means, std_devs, weights, y_true):
         # Create log likelihood for each Gaussian component
         log_likelihood = (
             -0.5 * (((y_true - means) ** 2) / (std_devs**2))
@@ -26,10 +23,4 @@ class MixtureGaussianNLLLoss(nn.Module):
         # Negative log likelihood for the Gaussian mixture
         nll = -log_likelihood_mixture
 
-        return nll
-
-    def forward(self, means, std_devs, weights, y_true):
-        # Apply softplus to std_devs for stability
-        std_devs = F.softplus(std_devs) + 1e-6  # 1e-6 to ensure numerical stability
-        gaussian_loss = self.gaussian_nll_loss(means, std_devs, weights, y_true).mean()
-        return gaussian_loss
+        return nll.mean()
