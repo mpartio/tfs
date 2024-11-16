139509f27cb76f0bf8cf2aca03dc6e356c9bfdb6
Mikko Partio <mikko.partio@fmi.fi>
2024-11-16 17:56:17 +0200

    Remove test-batch-size

--- config.py
+++ config.py
@@ -8,8 +8,6 @@ def get_args(mode = None):
     parser = argparse.ArgumentParser(description='PyTorch Example')
     parser.add_argument('--batch-size', type=int, default=4, metavar='N',
                         help='input batch size for training (default: 4)')
-    parser.add_argument('--test-batch-size', type=int, default=12, metavar='N',
-                        help='input batch size for testing (default: 12)')
     parser.add_argument('--epochs', type=int, default=100, metavar='N',
                         help='number of epochs to train (default: 100)')
     parser.add_argument('--dataset-size', type=str, default="10k", metavar='N',
