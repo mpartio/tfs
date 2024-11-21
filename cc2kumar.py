4adc7d7b1cd058ffd92ae216f1d4813797ef6ce4
Mikko Partio <mikko.partio@fmi.fi>
2024-11-21 12:07:55 +0200

    Network changes

--- cc2kumar.py
+++ cc2kumar.py
@@ -10,6 +10,8 @@ from layers import (
     MixtureBetaPredictionHead,
     MixtureBetaPredictionHeadLinear,
     MixtureBetaPredictionHeadWithConv,
+    PatchEmbeddingWithConv,
+    PatchRecoveryWithConv,
 )
 from reference_layers import (
     Downsample,
@@ -27,87 +29,92 @@ class CloudCastV2(nn.Module):
         if stride is None:
             stride = patch_size
 
-        self.patch_embed = PatchEmbedding(patch_size, dim, stride)
+        # self.patch_embed = PatchEmbedding(patch_size, dim, stride)
+        self.patch_embed = PatchEmbeddingWithConv(dim)
 
         self.downsample1 = Downsample(dim)
 
-#        self.residual_block1 = ResidualBlock(dim * 2)
+        #        self.residual_block1 = ResidualBlock(dim * 2)
 
-#        self.transformer_block1 = nn.ModuleList()
+        #        self.transformer_block1 = nn.ModuleList()
 
-#        for i in range(4):
-#            self.transformer_block1.append(
-#                SwinTransformerBlock(
-#                    dim=dim * 2,
-#                    window_size=5,
-#                    shift_size=((i % 2) * 2),
-#                    num_heads=4,
-#                    drop_path=0.05,
-#                )
-#            )
-#
-#        self.downsample2 = Downsample(dim * 2)
-#
-#        self.residual_block2 = ResidualBlock(dim * 4)
-#
-#        self.transformer_block2 = nn.ModuleList()
-#
-#        for i in range(6):
-#            self.transformer_block2.append(
-#                SwinTransformerBlock(
-#                    dim=dim * 4,
-#                    window_size=5,
-#                    shift_size=((i % 2) * 2),
-#                    num_heads=8,
-#                    drop_path=0.1,
-#                )
-#            )
-#
-#        self.conv_skip3 = ConvolvedSkipConnection(dim * 4)
-#
-#        self.upsample2 = Upsample(dim * 4)
-#
-#        self.residual_block3 = ResidualBlock(dim * 2)
-#
-#        self.transformer_block3 = nn.ModuleList()
-#
-#        for i in range(6):
-#            self.transformer_block3.append(
-#                SwinTransformerBlock(
-#                    dim=dim * 2,
-#                    window_size=5,
-#                    shift_size=((i % 2) * 2),
-#                    num_heads=8,
-#                    drop_path=0.1,
-#                )
-#            )
-#
-#        self.conv_skip2 = ConvolvedSkipConnection(dim * 2)
-#
+        #        for i in range(4):
+        #            self.transformer_block1.append(
+        #                SwinTransformerBlock(
+        #                    dim=dim * 2,
+        #                    window_size=5,
+        #                    shift_size=((i % 2) * 2),
+        #                    num_heads=4,
+        #                    drop_path=0.05,
+        #                )
+        #            )
+        #
+        #        self.downsample2 = Downsample(dim * 2)
+        #
+        #        self.residual_block2 = ResidualBlock(dim * 4)
+        #
+        #        self.transformer_block2 = nn.ModuleList()
+        #
+        #        for i in range(6):
+        #            self.transformer_block2.append(
+        #                SwinTransformerBlock(
+        #                    dim=dim * 4,
+        #                    window_size=5,
+        #                    shift_size=((i % 2) * 2),
+        #                    num_heads=8,
+        #                    drop_path=0.1,
+        #                )
+        #            )
+        #
+        #        self.conv_skip3 = ConvolvedSkipConnection(dim * 4)
+        #
+        #        self.upsample2 = Upsample(dim * 4)
+        #
+        #        self.residual_block3 = ResidualBlock(dim * 2)
+        #
+        #        self.transformer_block3 = nn.ModuleList()
+        #
+        #        for i in range(6):
+        #            self.transformer_block3.append(
+        #                SwinTransformerBlock(
+        #                    dim=dim * 2,
+        #                    window_size=5,
+        #                    shift_size=((i % 2) * 2),
+        #                    num_heads=8,
+        #                    drop_path=0.1,
+        #                )
+        #            )
+        #
+        #        self.conv_skip2 = ConvolvedSkipConnection(dim * 2)
+        #
         self.upsample1 = Upsample(dim * 2)
 
-#        self.residual_block4 = ResidualBlock(dim)
+        #        self.residual_block4 = ResidualBlock(dim)
 
-#        self.transformer_block4 = nn.ModuleList()
+        #        self.transformer_block4 = nn.ModuleList()
 
-#        for i in range(4):
-#            self.transformer_block4.append(
-#                SwinTransformerBlock(
-#                    dim=dim,
-#                    window_size=5,
-#                    shift_size=((i % 2) * 2),
-#                    num_heads=4,
-#                    drop_path=0.05,
-#                )
-#            )
+        #        for i in range(4):
+        #            self.transformer_block4.append(
+        #                SwinTransformerBlock(
+        #                    dim=dim,
+        #                    window_size=5,
+        #                    shift_size=((i % 2) * 2),
+        #                    num_heads=4,
+        #                    drop_path=0.05,
+        #                )
+        #            )
 
-#        self.conv_skip1 = ConvolvedSkipConnection(dim)
+        #        self.conv_skip1 = ConvolvedSkipConnection(dim)
 
-        self.patch_recover = PatchRecovery(
+        self.patch_recover = PatchRecoveryWithConv(
             dim=dim,
-            output_dim=3*num_mix, #dim,
-            patch_size=patch_size,
+            output_dim=3 * num_mix,
         )
+        # self.patch_recover = PatchRecovery(
+        #    dim=dim,
+        #    output_dim=3*num_mix, #dim,
+        #    patch_size=patch_size,
+        # )
 
         self.prediction_head = MixtureBetaPredictionHead(num_mix)
 
@@ -116,53 +123,52 @@ class CloudCastV2(nn.Module):
     def forward(self, x):
         # Reproject input data to latent space
         # From (B, T, H, W, C) to (B, T, H // patch, W // patch, C * 2)
-
         x = self.patch_embed(x)
 
         # Store the tensor for skip connection
-        skip1 = x
+        #skip1 = x
 
         # Downsample from (B, 32, 32, C) to (B, 16, 16, C*2)
         x = self.downsample1(x)
 
-#        x = self.residual_block1(x)
+        #        x = self.residual_block1(x)
 
-#        for block in self.transformer_block1:
-#            x = block(x)
+        #        for block in self.transformer_block1:
+        #            x = block(x)
 
-#        skip2 = x
+        #        skip2 = x
 
         # Downsample from (B, 16, 16, C*2) to (B, 8, 8, C*4)
-#        x = self.downsample2(x)
+        #        x = self.downsample2(x)
 
-#        skip3 = x
+        #        skip3 = x
 
-#        x = self.residual_block2(x)
+        #        x = self.residual_block2(x)
 
-#        for block in self.transformer_block2:
-#            x = block(x)
+        #        for block in self.transformer_block2:
+        #            x = block(x)
 
-#        x = self.conv_skip3(skip3, x)
+        #        x = self.conv_skip3(skip3, x)
 
         # Upsample from (B, 8, 8, C*4) to (B, 16, 16, C*2)
-#        x = self.upsample2(x)
+        #        x = self.upsample2(x)
 
-#        x = self.residual_block3(x)
+        #        x = self.residual_block3(x)
 
-#        for block in self.transformer_block3:
-#            x = block(x)
+        #        for block in self.transformer_block3:
+        #            x = block(x)
 
-#        x = self.conv_skip2(skip2, x)
+        #        x = self.conv_skip2(skip2, x)
 
         # Upsample from (B, 16, 16, 384) to (B, 32, 32, 192)
         x = self.upsample1(x)
 
-#        x = self.residual_block4(x)
+        #        x = self.residual_block4(x)
 
-#        for block in self.transformer_block4:
-#            x = block(x)
+        #        for block in self.transformer_block4:
+        #            x = block(x)
 
-#        x = self.conv_skip1(skip1, x)
+        #        x = self.conv_skip1(skip1, x)
 
         x = self.patch_recover(x)
         alpha, beta, weights = self.prediction_head(x)
