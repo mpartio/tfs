38d20b1836904898761d5170623da2b73617e609
Mikko Partio <mikko.partio@fmi.fi>
2025-01-16 16:47:55 +0000

    Fix mae logging

--- swinu-crps/cc2CRPS_callbacks.py
+++ swinu-crps/cc2CRPS_callbacks.py
@@ -71,12 +71,14 @@ class DiagnosticCallback(L.Callback):
 
             val_loss = np.nan if len(self.val_loss) == 0 else self.val_loss[-1]
 
+            mae = None if len(self.mae) == 0 else self.mae[-1]
+
             print(
                 "Iteration {:05d} Train Loss: {:.4f}, Val Loss: {:.4f}, L1: {:.4f}".format(
                     trainer.global_step,
                     self.train_loss[-1],
                     val_loss,
-                    self.mae[-1],
+                    mae,
                 )
             )
 
