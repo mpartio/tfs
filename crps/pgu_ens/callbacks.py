71430e12642b9f9adba9c24d7a8b05857fe3d118
Mikko Partio <mikko.partio@fmi.fi>
2025-07-15 10:33:16 +0300

    Remove manual history plotting and rely on mlflow
    Fine tune plots

--- crps/pgu_ens/callbacks.py
+++ crps/pgu_ens/callbacks.py
@@ -16,6 +16,8 @@ from dataclasses import asdict
 from matplotlib.ticker import ScalarFormatter
 from pytorch_lightning.loggers import CSVLogger
 from pytorch_lightning.utilities import rank_zero_only
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
 
 matplotlib.use("Agg")
 
@@ -385,14 +387,14 @@ class DiagnosticCallback(L.Callback):
             pl_module.log(
                 "val/snr_real",
                 snr_real["snr_db"],
-                on_step=True,
+                on_step=False,
                 on_epoch=True,
                 sync_dist=False,
             )
             pl_module.log(
                 "val/snr_pred",
                 snr_pred["snr_db"],
-                on_step=True,
+                on_step=False,
                 on_epoch=True,
                 sync_dist=False,
             )
@@ -432,88 +434,6 @@ class DiagnosticCallback(L.Callback):
             trainer.sanity_checking,
         )
 
-        return
-
-        current_val_loss = (
-            trainer.logged_metrics.get("val/loss_epoch", float("nan"))
-            .to(torch.float32)
-            .cpu()
-        )
-        current_lr = (
-            trainer.logged_metrics.get("lr", float("nan")).to(torch.float32).cpu()
-        )
-        current_snr_real = (
-            trainer.logged_metrics.get("val/snr_real", float("nan"))
-            .to(torch.float32)
-            .cpu()
-        )
-        current_snr_pred = (
-            trainer.logged_metrics.get("val/snr_pred", float("nan"))
-            .to(torch.float32)
-            .cpu()
-        )
-        current_var = trainer.logged_metrics.get("val/variance", float("nan"))
-        current_mae = trainer.logged_metrics.get("val/mae", float("nan"))
-
-        # Append to internal history lists
-        self.val_loss.append(current_val_loss)
-        self.lr.append(current_lr)
-        self.val_snr_real.append(current_snr_real)
-        self.val_snr_pred.append(current_snr_pred)
-        self.val_var.append(current_var)
-        self.val_mae.append(current_mae)
-
-        for k in self.loss_names:
-            val = trainer.logged_metrics.get(f"val/{k}_epoch", float("nan"))
-
-            try:
-                self.val_loss_components[k].append(val)
-            except KeyError:
-                self.val_loss_components[k] = [val]
-
-        self.plot_history(trainer.current_epoch, trainer.sanity_checking)
-
-    @rank_zero_only
-    def on_train_epoch_end(self, trainer, pl_module):
-        if trainer.sanity_checking:
-            return
-
-        return
-        current_train_loss = (
-            trainer.logged_metrics.get("train/loss_epoch", float("nan"))
-            .to(torch.float32)
-            .cpu()
-        )
-        self.train_loss.append(current_train_loss)
-
-        current_var = trainer.logged_metrics.get("train/variance", float("nan"))
-        current_mae = trainer.logged_metrics.get("train/mae", float("nan"))
-        self.train_var.append(current_var)
-        self.train_mae.append(current_mae)
-
-        for k in self.loss_names:
-            val = trainer.logged_metrics.get(f"train/{k}_epoch", float("nan")).cpu()
-
-            try:
-                self.train_loss_components[k].append(val)
-            except KeyError:
-                self.train_loss_components[k] = [val]
-
-        for k in self.grad_names:
-            val = trainer.logged_metrics.get(f"train/grad_{k}_mean_epoch", float("nan"))
-
-            try:
-                self.gradients_mean[k].append(val)
-            except KeyError:
-                self.gradients_mean[k] = [val]
-
-            val = trainer.logged_metrics.get(f"train/grad_{k}_std_epoch", float("nan"))
-
-            try:
-                self.gradients_std[k].append(val)
-            except KeyError:
-                self.gradients_std[k] = [val]
-
     def plot_visual(
         self, input_field, truth, pred, tendencies, epoch, sanity_checking=False
     ):
@@ -537,22 +457,31 @@ class DiagnosticCallback(L.Callback):
         cmap = plt.cm.coolwarm
         norm = mcolors.TwoSlopeNorm(vmin=-1, vcenter=0, vmax=1)
 
+        row_imshow = [None] * T
+
         for t in range(T):
             if t == 0:
                 true_tendencies = truth[t].squeeze() - input_field[-1].squeeze()
             else:
                 true_tendencies = truth[t].squeeze() - truth[t - 1].squeeze()
 
-            im = ax[t, 0].imshow(true_tendencies, cmap=cmap, norm=norm)
+            im0 = ax[t, 0].imshow(true_tendencies, cmap=cmap, norm=norm)
             ax[t, 0].set_title(f"True tendencies step={t}")
+            ax[t, 0].set_xticks([])
+            ax[t, 0].set_yticks([])
 
-            im = ax[t, 1].imshow(tendencies[0][t], cmap=cmap, norm=norm)
+            im1 = ax[t, 1].imshow(tendencies[0][t], cmap=cmap, norm=norm)
             ax[t, 1].set_title(f"Predicted tendencies m=0 step={t}")
+            ax[t, 1].set_xticks([])
+            ax[t, 1].set_yticks([])
 
             data = true_tendencies - tendencies[0][t]
+            im2 = ax[t, 2].imshow(data.cpu(), cmap=cmap, norm=norm)
             ax[t, 2].set_title(f"Tendencies bias m=0 step={t}")
-            im = ax[t, 2].imshow(data.cpu(), cmap=cmap, norm=norm)
-            fig.colorbar(im, ax=ax[t, 2])
+            ax[t, 2].set_xticks([])
+            ax[t, 2].set_yticks([])
+
+            row_imshow[t] = im2
 
             ax[t, 3].set_title(f"True histogram step={t}")
             ax[t, 3].hist(true_tendencies.flatten(), bins=30)
@@ -560,12 +489,23 @@ class DiagnosticCallback(L.Callback):
             ax[t, 4].set_title(f"Predicted histogram m=0 step={t}")
             ax[t, 4].hist(tendencies[0][t].flatten(), bins=30)
 
-            uncertainty = torch.var(pred[:, t], dim=0)
-            plt.title("Tendency Variance")
-            plt.imshow(uncertainty.cpu(), cmap="Reds")
-            plt.colorbar()
+            uncertainty = torch.var(pred[:, t], dim=0).cpu()
+            imv = ax[t, 5].imshow(uncertainty, cmap="Reds")
+            ax[t, 5].set_title("Tendency Variance")
+            ax[t, 5].set_xticks([])
+            ax[t, 5].set_yticks([])
 
-        plt.tight_layout()
+            # add a small colorbar for the variance plot
+            divider = make_axes_locatable(ax[t, 5])
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            fig.colorbar(imv, cax=cax)
+
+        for t in range(T):
+            divider = make_axes_locatable(ax[t, 2])
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            fig.colorbar(row_imshow[t], cax=cax)
+
+        plt.tight_layout(rect=[0, 0, 1, 0.97])  # leave room for suptitle
 
         if sanity_checking:
             return
@@ -581,159 +521,7 @@ class DiagnosticCallback(L.Callback):
         )
 
         plt.savefig(
-            "{}/figures/{}_{}_{}_epoch_{:03d}_val.png".format(
-                run_dir,
-                platform.node(),
-                run_name,
-                run_number,
-                epoch,
-            )
-        )
-
-        plt.close()
-
-    def plot_history(self, epoch, sanity_checking=False):
-        warnings.filterwarnings(
-            "ignore", message="No artists with labels found to put in legend"
-        )
-
-        plt.figure(figsize=(20, 8))
-        plt.suptitle(
-            "{} at epoch {} (host={}, time={})".format(
-                sys.argv[0],
-                epoch,
-                platform.node(),
-                datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            )
-        )
-        run_name, run_number, run_dir = run_info()
-
-        train_loss = self.train_loss
-        if len(train_loss) > 2000:
-            # Remove first 100 after we have enough data, as they often
-            # they contain data messes up the y-axis
-            train_loss = train_loss[100:]
-
-        plt.subplot(241)
-        plt.title("Training loss")
-        plt.plot(train_loss, color="blue", alpha=0.3)
-        plt.plot(
-            moving_average(torch.tensor(self.train_loss), 60),
-            color="blue",
-            label="Train Loss",
-        )
-        plt.legend(loc="upper left")
-
-        ax2 = plt.gca().twinx()
-        ax2.plot(torch.tensor(self.lr), label="LR", color="green")
-        ax2.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
-        ax2.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-
-        ax2.legend(loc="upper right")
-
-        val_loss = self.val_loss
-        if len(val_loss) > 500:
-            val_loss = val_loss[20:]
-
-        plt.subplot(242)
-        plt.plot(val_loss, color="orange", alpha=0.3)
-        plt.plot(
-            moving_average(torch.tensor(self.val_loss), 60),
-            color="orange",
-            label="Val Loss",
-        )
-        plt.title("Validation loss")
-        plt.legend(loc="upper left")
-
-        plt.subplot(243)
-        snr_real = torch.tensor(self.val_snr_real)
-        snr_pred = torch.tensor(self.val_snr_pred)
-        plt.plot(snr_real, color="blue", alpha=0.3)
-        plt.plot(
-            moving_average(snr_real, 30),
-            color="blue",
-            label="Real",
-        )
-        plt.plot(snr_pred, color="orange", alpha=0.3)
-        plt.plot(moving_average(snr_pred, 30), color="orange", label="Predicted")
-        plt.legend(loc="upper left")
-
-        ax2 = plt.gca().twinx()
-        residual = snr_real - snr_pred
-
-        ax2.plot(residual, color="green", alpha=0.3)
-        ax2.plot(moving_average(residual, 30), label="Residual", color="green")
-        ax2.legend(loc="upper right")
-        plt.title("Signal to Noise Ratio")
-
-        plt.subplot(244)
-        plt.yscale("log")
-        plt.title("Gradients (mean)")
-
-        for i, section in enumerate(self.gradients_mean.keys()):
-            data = self.gradients_mean[section]
-            data = torch.tensor(data)
-            color = colors[i]
-            plt.plot(data, color=color, alpha=0.3)
-            plt.plot(moving_average(data, 30), color=color, label=section)
-        plt.legend()
-
-        plt.subplot(245)
-        plt.yscale("log")
-        plt.title("Gradients (std)")
-
-        for i, section in enumerate(self.gradients_std.keys()):
-            data = self.gradients_std[section]
-            data = torch.tensor(data)
-            color = colors[i]
-            plt.plot(data, color=color, alpha=0.3)
-            plt.plot(moving_average(data, 30), color=color, label=section)
-        plt.legend()
-
-        plt.subplot(246)
-        train_var = torch.tensor(self.train_var).cpu()
-        plt.plot(train_var, color="blue", alpha=0.3)
-        plt.plot(
-            moving_average(train_var, 30),
-            color="blue",
-            label="Variance",
-        )
-        plt.legend(loc="upper left")
-
-        train_mae = torch.tensor(self.train_mae).cpu()
-        ax2 = plt.gca().twinx()
-        ax2.plot(train_mae, color="orange", alpha=0.3)
-        ax2.plot(
-            moving_average(train_mae, 30),
-            color="orange",
-            label="MAE",
-        )
-        ax2.legend(loc="upper right")
-        plt.title("Train Variance vs MAE")
-
-        val_var = torch.tensor(self.val_var).cpu()
-        plt.subplot(247)
-        plt.plot(val_var, color="blue", alpha=0.3)
-        plt.plot(
-            moving_average(val_var, 30),
-            color="blue",
-            label="Variance",
-        )
-        plt.legend(loc="upper left")
-
-        val_mae = torch.tensor(self.val_mae)
-        ax2 = plt.gca().twinx()
-        ax2.plot(val_mae, color="orange", alpha=0.3)
-        ax2.plot(moving_average(val_mae, 30), color="orange", label="MAE")
-        ax2.legend(loc="upper right")
-        plt.title("Val Variance vs MAE")
-
-        plt.tight_layout()
-        if sanity_checking:
-            return
-
-        plt.savefig(
-            "{}/figures/{}_{}_{}_epoch_{:03d}_history.png".format(
+            "{}/figures/{}_{}_{}_epoch_{:03d}_visual.png".format(
                 run_dir,
                 platform.node(),
                 run_name,
