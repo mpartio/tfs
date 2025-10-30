7adf1364a730c80b952b009d9b05cd1073a7df36
Mikko Partio <mikko.partio@fmi.fi>
2025-10-30 15:32:01 +0000

    Update example script

--- crps/run-batch-infer.sh
+++ crps/run-batch-infer.sh
@@ -1,21 +1,33 @@
-set -xu
-export MODEL_FAMILY=pgu
-export RUN_NAME=other-coda
-export RUN_NUMBER=1
+set -xe
+
+if [ -z "$RUN_NAME" ]; then
+  export RUN_NAME=wary-voice
+  export RUN_NUMBER=1
+fi
+
 export ROLLOUT_LENGTH=5
+
+if [ -f "/data/runs/$RUN_NAME/$RUN_NUMBER/checkpoints/best.ckpt" ]; then
+  ckpt=/data/runs/$RUN_NAME/$RUN_NUMBER/checkpoints/best.ckpt
+else
+  ckpt=$(ls /data/runs/$RUN_NAME/$RUN_NUMBER/checkpoints/ep* | sort -t= -k2 -n | tail -1)
+fi
 
 python3 cc2trainer.py test \
 	--config /data/runs/$RUN_NAME/$RUN_NUMBER/config.yaml \
-	--ckpt_path /data/runs/$RUN_NAME/$RUN_NUMBER/checkpoints/last.ckpt \
+	--ckpt_path $ckpt \
 	--model.init_weights_from_ckpt=False \
 	--model.adapt_ckpt_resolution=False \
-	--data.data_path='["../data/nwcsaf-475x535-202503-anemoi.zarr"]' \
+	--data.data_path='["/anemoi-data/nwcsaf-475x535-202507-v3-anemoi.zarr"]' \
 	--trainer.logger=False \
 	--trainer.devices=1 \
+	--trainer.num_nodes=1 \
 	--trainer.strategy=auto \
-	--data.test_split=1 \
-	--data.val_split=0 \
-	--data.batch_size=8 \
-	--model.rollout_length=$ROLLOUT_LENGTH \
-	--data.rollout_length=$ROLLOUT_LENGTH
-
+	--data.train_start=null \
+	--data.train_end=null \
+	--data.val_start=null \
+	--data.val_end=null \
+	--data.test_start="2025-07-01" \
+	--data.test_end="2025-08-01" \
+	--data.batch_size=2 \
+	--model.rollout_length=$ROLLOUT_LENGTH
--- crps/run-verify.sh
+++ crps/run-verify.sh
@@ -1,8 +1,7 @@
 set -xu
 
-runs="nwcsaf-rollout-1 trite-cabin cloudcast-production" 
+runs="hushed-volume/2 wary-voice/1 weathered-heron/1 seasoned-pass/1 excited-bass/1 "
 
 python3 verify.py \
 	--run_name $runs \
-	--score mae mae2d psd
-
+	--score mae mae2d psd fss variance_ratio highk_power_ratio spectral_coherence change_metrics
