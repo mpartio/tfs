2eca21e475de1ff3511af706e22e01bca373a9bc
Mikko Partio <mikko.partio@fmi.fi>
2025-11-12 14:30:18 +0000

    Bugfix

--- crps/cc2CRPS_L.py
+++ crps/cc2CRPS_L.py
@@ -503,6 +503,6 @@ class cc2CRPSModel(L.LightningModule):
         dm = self.trainer.datamodule
         if dm and dm._full_dataset and hasattr(dm._full_dataset, "data"):
             checkpoint["data_statistics"] = dm._full_dataset.data.statistics
-            checkpoint["data_statististics_name_to_index"] = dm._full_dataset.name_to_index
+            checkpoint["data_statististics_name_to_index"] = dm._full_dataset.data.name_to_index
         else:
             rank_zero_warn("Statistics not saved to checkpoint")
