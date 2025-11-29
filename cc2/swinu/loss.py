53ad46c570ff4645cd6b529d24715539575e506e
Partio Mikko <mpartio@gmail.com>
2025-11-29 16:16:34 +0200

    Add MAE

--- cc2/swinu/loss.py
+++ cc2/swinu/loss.py
@@ -19,6 +19,16 @@ class MSELoss(nn.Module):
         return mse_loss(y_true, y_pred)
 
 
+class MAELoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, y_true: torch.Tensor, y_pred: torch.Tensor):
+        loss = torch.abs(y_true - y_pred).mean()
+        assert torch.isfinite(loss).all(), "Non-finite values at loss: {}".format(loss)
+        return {"loss": loss, "mae_loss": loss}
+
+
 LOSS_FUNCTIONS = {
     "mse_loss": mse_loss,
     "mse_plus_amse_loss": mse_plus_amse_loss,
